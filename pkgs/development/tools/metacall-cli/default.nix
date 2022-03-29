bfedad3bfbf3c7ed4340741125d5d4557646c908
Lahfa Samy <samy@lahfa.xyz>
2022-03-29 23:26:09 +0200

    Update metacall WIP nix, fetchFromGitHub now to fix most warnings

--- pkgs/development/tools/metacall-cli/default.nix
+++ pkgs/development/tools/metacall-cli/default.nix
@@ -1,15 +1,18 @@
-{lib, stdenv, fetchurl, cmake,
+{lib, stdenv, fetchFromGitHub, cmake, extra-cmake-modules, git, rapidjson, makeWrapper,
 withPy, withNode, withTypescript, withC, withRuby, withJava, withCobalt}:
 
 stdenv.mkDerivation rec {
-  pname = "metacall-cli";
+  pname = "metacall";
   version = "0.5.7";
-
-  src = fetchurl {
-    url = "https://github.com/metacall/core/archive/v${version}.tar.gz";
-    sha256 = "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i";
+  #hardeningDisable = [ "all" ];
+  cmakeFlags = [ "-Wno-dev" "-DOPTION_BUILD_DETOURS=off" "-DOPTION_FORK_SAFE=off" "-DOPTION_BUILD_TESTS=off"];
+  NIX_CFLAGS_COMPILE = ["-Wno-error" ]; # "-Wno-stringop-truncation" "-Wno-dev" "-Wno-stringop-overread" "-Wno-stringop-overflow"];
+  src = fetchFromGitHub {
+    owner = "metacall";
+    repo = "core";
+    rev = "adcc50496d53797011b87f42131cb857d0009ffb";
+    sha256 = "13cfyswmjbdbwl17qb639mdd0x2vm1k96kmkh4bs3fzhvbrw43r5";
   };
-
   # Support for loaders disabled by default
   # Unless user pkg overrides
   withNode = false;
@@ -21,27 +24,36 @@ stdenv.mkDerivation rec {
   withC = false;
   withTypescript = false;
 
-  nativebuildInputs = [ cmake ];
+  buildInputs = [ cmake extra-cmake-modules git rapidjson ];
+  nativeBuildInputs = [ makeWrapper ];
   # TODO: List all needed dependencies for each loaders support
-  buildInputs = lib.optionals withTypescript [ nodePackages.typescript ] 
-  ++ lib.optionals withPy [ python3Full ]
-  ++ lib.optionals withNode [ nodejs ]
-  ++ lib.optionals withJS []
-  ++ lib.optionals withC []
-  ++ lib.optionals withRuby []
-  ++ lib.optionals withJava []
-  ++ lib.optionals withCobalt [];
+  #buildInputs = lib.optionals withTypescript [ nixpkgs.nodePackages.typescript ] 
+  #++ lib.optionals withPy [ nixpkgs.python3Full ]
+  #++ lib.optionals withNode [ nixpkgs.nodejs ]
+  ##++ lib.optionals withJS [ nixpkgs.nodejs ];
+ # ++ lib.optionals withC []
+ # ++ lib.optionals withRuby []
+ # ++ lib.optionals withJava []
+ # ++ lib.optionals withCobalt [];
   # TODO: Add optionals CmakeFlags
-  doCheck = true; # Run cmake tests
-
-
+  doCheck = true;
+  
+  postInstall = ''
+        makeWrapper $out/metacallcli $out/metacall \
+      --prefix LOADER_LIBRARY_PATH : "$out/lib/" \
+      --prefix SERIAL_LIBRARY_PATH : "$out/lib" \
+      --prefix DETOUR_LIBRARY_PATH : "$out/lib" \
+      --prefix PORT_LIBRARY_PATH : "$out/lib" \
+      --prefix CONFIGURATION_PATH: "$out/configurations/globals.json" \
+  '';
+  #--prefix PYTHONPATH : "${lib.makeLibraryPath libraries}" \
   meta = with lib; {
     description = "A CLI for the library providing inter-language foreign function interface calls";
     homepage = "https://metacall.io/";
     changelog = "https://github.com/metacall/core/releases/tag/v${version}";
     license = licenses.asl20; # Apache 2.0
     maintainers = with maintainers; [ akechishiro ]; 
-    platforms = platforms.all;
+    platforms = platforms.linux;
+    mainProgram = "metacall";
   };
 }
-
